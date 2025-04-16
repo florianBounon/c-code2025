a8c45c34639d2882fb318bdb88fc18dd21a3fd6c
florianBounon <florian.bounon@edu.esiee-it.fr>
2025-04-16 11:48:09 +0200

    Add complexity constraints to questionnaires

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -13,7 +13,7 @@ class ApiController extends Controller
     {
         $request->validate([
             'language' => 'required|string',
-            'difficulty' => 'required|string|in:facile,intermédiaire,difficile',
+            'difficulty' => 'required|string|in:Easy,Intermediate,hard',
             'questionsCount' => 'required|integer|min:2',
             'reponsesCount' => 'required|integer|min:3',
         ], [
@@ -26,13 +26,41 @@ class ApiController extends Controller
         $questionsCount = $request->input('questionsCount');
         $reponsesCount = $request->input('reponsesCount');
 
+
+        // Prevent overloading the questionnaire
+        $complexite = ($request->input('questionsCount') * 1.25) * $request->input('reponsesCount');
+        $complexiteMax = 125;
+        if($difficulty == "Intermediate") {
+            $complexite *= 1.20;
+        }elseif($difficulty == "hard") {
+            $complexite *= 1.40;
+        }
+        if ($complexite > $complexiteMax) {
+            return redirect()->back()
+                ->withInput()
+                ->withErrors([
+                    'questionsCount' => "La complexité de generation du questionnaire est trop élevée.",
+                ]);
+        }
+
+
+        
         $prompt = <<<EOD
         Créer un questionnaire sur le langage de programmation $language avec un niveau de difficulté $difficulty.
         Le questionnaire doit contenir $questionsCount questions,
         et chaque question doit avoir $reponsesCount réponses possibles.
-        Chaque question doit être suivie des réponses proposées.
-        Affiche les réponses après les question, sous la forme : réponse "numéro de la question" : "bonne réponse".
+
+        Affiche les questions sous la forme suivante sans afiche les bonnes réponses:  
+        question "numéro de la question" : "coeficien de dificulter (de 0 a 1)" : "intitulé de la question"  
+        réponse "numéro de la réponse" : "contenu de la réponse"  
+        réponse "numéro de la réponse" : "contenu de la réponse" 
+        ...
         
+        Une fois toutes les questions affichées, affiche les bonnes réponses à la fin, sous la forme suivante :
+        réponse question "numéro de la question" : réponse "numéro de la bonne réponse" : "réponse"
+        
+        Enfin, ajoute un lien vers une image représentant le langage utilisé dans le questionnaire, sous la forme :  
+        image : <"lien de l'image">
         
 EOD;
 
